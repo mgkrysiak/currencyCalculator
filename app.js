0c54386bace4c363f332fb3a91b07312fa2427a4
mgkrysiak <gabi.krysiak@gmail.com>
2018-12-13 23:11:47 +0100

    last corrections, finished validation and rounding numbers

--- app.js
+++ app.js
@@ -25,9 +25,9 @@ document.addEventListener("DOMContentLoaded", function() {
 		} else if (from === 'PLN' && to === 'USD') {
 			result = amount * PLNUSD;
 		} else if (from === 'EUR' && to === 'PLN') {
-			result = amount * EURPLN;
+			Math.round(result = amount * EURPLN * 100 / 100)
 		} else if (from === 'PLN' && to === 'EUR') {
-			result = amount * PLNEUR;
+			Math.round(result = amount * PLNEUR * 100 / 100);
 		} else if (from === 'GBP' && to === 'PLN') {
 			result = amount * GBPPLN;
 		} else if (from === 'PLN' && to === 'GBP') {
@@ -38,8 +38,8 @@ document.addEventListener("DOMContentLoaded", function() {
 			result = amount * PLNCZK;
 		}
 
-		showResult.textContent = result + ' ' + to;
-		return true;
+		showResult.textContent = parseFloat(result).toFixed(2) + ' ' + to;
+
 
 	}
 
@@ -49,30 +49,37 @@ document.addEventListener("DOMContentLoaded", function() {
 		var from = firstCurrency.value;
 		var to = secondCurrency.value;
 		var amount = amountInput.value;
+		var formCorrect = true;
 
 		if (from == to) {
 			form.preventDefault();
 			alert('Currencies must be different');
-			return false;
+			formCorrect = false;
 		}
 		else if (amount <= 0) {
 			form.preventDefault();
 			alert('Please input proper amount');
-			return false;
+			formCorrect = false;
 		}
 		else if (from !== 'PLN' && to !== 'PLN') {
 			form.preventDefault();
 			alert('Choose PLN as one of options');
-			return false;
+			formCorrect = false;
 		}
 		else if (/\D/.test(amount)) {
 			form.preventDefault();
 			alert('Please input only numbers');
+			formCorrect = false;
+		}
+		else if (formCorrect = true) {
+			convertCurrency(form);
+		}
+		else if (formCorrect = false) {
+			showResult.innerHTML = '';
 		}
 
 
 	}
 	btn.addEventListener('click', validate)
-  btn.addEventListener('click', convertCurrency);
 
 });
